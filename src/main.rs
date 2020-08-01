2c4d84d98057588abe794688d14281b235ad2a4f
david-sk <david.desmeurs@slagkryssaren.com>
2020-08-01 23:41:59 +0200

    Fixy!

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,8 @@ mod problem_13_v1;
 mod problem_14_v1;
 #[path = "problems/0014_longest_collatz_sequence/v2.rs"]
 mod problem_14_v2;
-#[path = "problems/0024_lattice_paths/v1.rs"]
-mod problem_24_v1;
+#[path = "problems/0015_lattice_paths/v1.rs"]
+mod problem_15_v1;
 #[path = "problems/0055_lychrel_numbers/v1.rs"]
 mod problem_55_v1;
 #[path = "problems/0059_xor_decryption/v1.rs"]
@@ -96,7 +96,7 @@ fn main() {
         ("13", "v1") => problem_13_v1::run(),
         ("14", "v1") => problem_14_v1::run(),
         ("14", "v2") => problem_14_v2::run(),
-        ("24", "v1") => problem_24_v1::run(),
+        ("15", "v1") => problem_15_v1::run(),
         ("55", "v1") => problem_55_v1::run(),
         ("59", "v1") => problem_59_v1::run(),
         ("179", "v1") => problem_179_v1::run(),
