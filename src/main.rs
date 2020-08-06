6c61c29bd853e83ac7dc88d99e9af04b67201590
david-sk <david.desmeurs@slagkryssaren.com>
2020-08-06 20:33:47 +0200

    Problem 41, Rust!

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,8 @@ mod problem_15_v1;
 mod problem_18_v1;
 #[path = "problems/0031_coins_sum/v1.rs"]
 mod problem_31_v1;
+#[path = "problems/0041_pandigital_prime/v1.rs"]
+mod problem_41_v1;
 #[path = "problems/0043_sub_string_divisibility/v3.rs"]
 mod problem_43_v3;
 #[path = "problems/0055_lychrel_numbers/v1.rs"]
@@ -107,6 +109,7 @@ fn main() {
         ("15", "v1") => problem_15_v1::run(),
         ("18", "v1") => problem_18_v1::run(),
         ("31", "v1") => problem_31_v1::run(),
+        ("41", "v1") => problem_41_v1::run(),
         ("43", "v3") => problem_43_v3::run(),
         ("55", "v1") => problem_55_v1::run(),
         ("59", "v1") => problem_59_v1::run(),
---
+++ src/problems/0041_pandigital_prime/v1.rs
@@ -0,0 +1,76 @@
+//
+// Pandigital prime, v1
+// https://projecteuler.net/problem=41
+//
+// We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n
+// exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+// What is the largest n-digit pandigital prime that exists?
+//
+
+mod prime;
+
+use prime::is_prime;
+
+// Note that this takes the next _lower_ permutation of digits
+fn next_permutation(array: &mut [u64], length: usize) -> bool {
+    let mut i: usize = length - 1;
+    while i > 0 && array[i] >= array[i - 1] {
+        i -= 1;
+    }
+    if i == 0 {
+        return false;
+    }
+
+    let mut j: usize = i - 1;
+    let mut k: usize = length - 1;
+    while array[k] > array[j] {
+        k -= 1;
+    }
+    let tmp = array[j];
+    array[j] = array[k];
+    array[k] = tmp;
+
+    j = length - 1;
+    k = i;
+    while k < j {
+        let tmp = array[j];
+        array[j] = array[k];
+        array[k] = tmp;
+        k += 1;
+        j -= 1;
+    }
+
+    return true;
+}
+
+fn get_digits_as_number(digits: &mut [u64], length: usize) -> u64 {
+    let mut number = 0;
+    let mut ten_powers = 1;
+    for i in 0..length {
+        number += digits[length - i - 1] * ten_powers;
+        ten_powers *= 10;
+    }
+    return number;
+}
+
+fn is_digits_number_prime(digits: &mut [u64], length: usize) -> bool {
+    return is_prime(get_digits_as_number(digits, length));
+}
+
+pub fn run() {
+    const TOTAL_LENGTH: usize = 9;
+    let mut digits: [u64; TOTAL_LENGTH] = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+    let mut length = TOTAL_LENGTH;
+
+    while !is_digits_number_prime(&mut digits, length) {
+        if !next_permutation(&mut digits, length) && length > 1 {
+            length -= 1;
+            for i in 0..length {
+                digits[i] = (length - i) as u64;
+            }
+        }
+    }
+
+    let largest_pandigital_prime = get_digits_as_number(&mut digits, length);
+    println!("Largest pandigital prime: {}", largest_pandigital_prime);
+}
