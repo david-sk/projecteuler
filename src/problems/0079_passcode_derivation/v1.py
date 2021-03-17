b30cfdbbb3c9ca8aa88b8dc5cbce1421782d3419
david-sk <david.desmeurs@slagkryssaren.com>
2021-03-17 23:59:24 +0100

    Avoiding duplicates in attempts.

--- src/problems/0079_passcode_derivation/v1.py
+++ src/problems/0079_passcode_derivation/v1.py
@@ -9,68 +9,71 @@
 # Given that the three characters are always asked for in order, analyse the file so as to
 # determine the shortest possible secret passcode of unknown length.
 #
-# The keylog.txt content is directly put into `ATTEMPTS`
+# NOTE: The keylog.txt content is directly put into `ATTEMPTS` where duplicates
+#       are auto removed as the attempts are being introduced as a set.
 #
 
 from itertools import permutations
 
-ATTEMPTS = [
-    '319',
-    '680',
-    '180',
-    '690',
-    '129',
-    '620',
-    '762',
-    '689',
-    '762',
-    '318',
-    '368',
-    '710',
-    '720',
-    '710',
-    '629',
-    '168',
-    '160',
-    '689',
-    '716',
-    '731',
-    '736',
-    '729',
-    '316',
-    '729',
-    '729',
-    '710',
-    '769',
-    '290',
-    '719',
-    '680',
-    '318',
-    '389',
-    '162',
-    '289',
-    '162',
-    '718',
-    '729',
-    '319',
-    '790',
-    '680',
-    '890',
-    '362',
-    '319',
-    '760',
-    '316',
-    '729',
-    '380',
-    '319',
-    '728',
-    '716',
-]
+ATTEMPTS = set(
+    [
+        '319',
+        '680',
+        '180',
+        '690',
+        '129',
+        '620',
+        '762',
+        '689',
+        '762',
+        '318',
+        '368',
+        '710',
+        '720',
+        '710',
+        '629',
+        '168',
+        '160',
+        '689',
+        '716',
+        '731',
+        '736',
+        '729',
+        '316',
+        '729',
+        '729',
+        '710',
+        '769',
+        '290',
+        '719',
+        '680',
+        '318',
+        '389',
+        '162',
+        '289',
+        '162',
+        '718',
+        '729',
+        '319',
+        '790',
+        '680',
+        '890',
+        '362',
+        '319',
+        '760',
+        '316',
+        '729',
+        '380',
+        '319',
+        '728',
+        '716',
+    ]
+)
 
 
 def is_possible(passcode):
     for a, b, c in ATTEMPTS:
-        if a not in passcode or b not in passcode or c not in passcode:
+        if a not in passcode or c not in passcode:
             return False
         a_index, c_index = passcode.index(a), passcode.rindex(c)
         if b not in passcode[a_index + 1 : c_index]:
