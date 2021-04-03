8e63f147222f435a3c55f47e67c0a108ed5036af
david-sk <david.desmeurs@slagkryssaren.com>
2021-04-03 12:51:34 +0200

    Nicer printing!

--- src/problems/0076_counting_summations/v1.py
+++ src/problems/0076_counting_summations/v1.py
@@ -34,10 +34,17 @@ def find_summations(n):
 def run():
     # Well, this algorithm version is way too slow to find the solution for 100.
     # But let's cheat! Execute this to have the 2 to 30 sequence printed in reasonable time:
-    for n in range(2, 31):
-        print(len(find_summations(n)), end=', ' if n < 30 else '')
-    print()
-    # Then copy the comma separated numbers, go to http://sequencedb.net/, paste the numbers, which
-    # should find a solution for that sequence. From solution http://sequencedb.net/s/hwz12ospcif4b,
-    # scroll down a bit and take the value corresponding to 100, that is, 190569291... TADA!
 
+    sequence = ''
+    for n in range(2, 31):
+        sequence += f"{len(find_summations(n))}{', ' if n < 30 else ''}"
+
+    print('Sequence for 2 to 30:')
+    print(sequence, '\n')
+    print('Go find the solution for 100 at:', '\n')
+    print('http://sequencedb.net/index.html?s=' + sequence.replace(' ', '+').replace(',', '%2C'))
+
+    # Then either go to the printed link, or copy/paste the comma separated numbers in
+    # http://sequencedb.net/. This sequence database website should find a solution for that
+    # sequence. For example, from solution at http://sequencedb.net/s/hwz12ospcif4b, scroll
+    # down a bit and take the value corresponding to 100, that is, 190569291... TADA!
